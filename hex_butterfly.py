1191a7c4307709688b32118e16a2da4620f5a922
Dan Piponi <dpiponi@gmail.com>
2013-01-15 07:21:22 -0800

    Nice butterflies

--- hex_butterfly.py
+++ hex_butterfly.py
@@ -1,11 +1,5 @@
 from nano import *
 
-RECT_RIBBON = r"""
-A-B-C-D-E-F-G-H-I-J-K-L-M-N-O-P-Q-R-S-T-U-V-W-X-Y-Z-a-b-c-d-e-f-g-h-i-j-k-l-m-n-p-q-r-s-t-u-v-w-x-y-z
-| | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | |
-A B C D E F G H I J K L M N O P Q R S T U V W X Y Z a b c d e f g h i j k l m n p q r s t u v w x y z
-"""
-
 HEX_RIBBON = r"""
   A-B     C-D     E-B     G-H     I-J     K-L     M-N     O-P     Q-R     S-T     U-V     W-X     Y-Z     a-b    
  /   \   /   \   /   \   /   \   /   \   /   \   /   \   /   \   /   \   /   \   /   \   /   \   /   \   /   \    
@@ -17,15 +11,13 @@ o     o-o     o-o     o-o     o-o     o-o     o-o     o-o     o-o     o-o     o-
 dx = units.nanometre
 dy = units.nanometre
 
-#num_atoms, dimension, bonds, atoms = parse_diagram(RECT_RIBBON)
 num_atoms, dimension, bonds, atoms = parse_diagram(HEX_RIBBON)
 
-n = 1024
+n = 64
 x = numpy.zeros((n, num_atoms*num_atoms), dtype = numpy.float32)
 y = numpy.zeros((n, num_atoms*num_atoms), dtype = numpy.float32)
 for l in range(0, n):
     print l
-    B = 0.5*math.pi*units.hbar/units.electron_charge*float(l)/n # rect
     B = 0.25*0.5*math.pi*units.hbar/units.electron_charge*float(l)/n # hex
     h_poly = compute_hamiltonian(num_atoms, atoms, bonds, B)
     p = 0
@@ -45,8 +37,6 @@ for l in range(0, n):
 
 for m in range(0, num_atoms*num_atoms):
     matplotlib.pyplot.plot(range(n), y[:, m], 'k', linewidth = 0.05)
-#matplotlib.pyplot.hexbin(x.flatten(),y.flatten(),gridsize=512)
 
 #matplotlib.pyplot.show()
-#matplotlib.pyplot.savefig("rect.1000.jpg", dpi = 1000)
 matplotlib.pyplot.savefig("hex.1000.jpg", dpi = 1000)
